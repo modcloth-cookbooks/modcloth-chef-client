ac9727fe35ac3689b3ab73068fa6ecab5bad38a3
Dan Buch <d.buch@modcloth.com>
2013-08-30 21:05:26 -0400

    Adding Travis integration and cleaning up foodcritic errors

---
+++ README.md
@@ -0,0 +1,4 @@
+`modcloth-chef-client` cookbook
+===============================
+
+ModCloth's take on running `chef-client`!
--- metadata.rb
+++ metadata.rb
@@ -3,4 +3,5 @@ maintainer        "ModCloth, Inc."
 maintainer_email  "platformsphere@modcloth.com"
 license           "All Rights Reserved"
 description       "Manages aspects chef-client"
+long_description  File.read(File.expand_path('../README.md', __FILE__))
 version           "0.1.0"
